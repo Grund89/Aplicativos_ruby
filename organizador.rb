7ecdcb5eb51d3a30a089d6ee0c9a8b9c7d33b9df
Douglas Grund <douglasgrund@yahoo.com.br>
2024-11-01 02:21:13 -0300

    feat: cria classe cliente

--- organizador.rb
+++ organizador.rb
@@ -1,1 +1,13 @@
 # Organizar uma fila de atendimentos
+
+class Cliente
+  attr_accessor :id, :tempo_de_chegada, :tempo_de_atendimento, :tempo_de_inicio, :tempo_de_saida
+
+  def initialize(id, tempo_de_chegada, tempo_de_atendimento)
+    @id = id
+    @tempo_de_chegada = tempo_de_chegada
+    @tempo_de_atendimento = tempo_de_atendimento
+    @tempo_de_inicio = nil  # Quando o atendimento realmente começa
+    @tempo_de_saida = nil   # Quando o atendimento realmente termina
+  end
+end
